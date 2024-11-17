56e65b66eda8aa04bce40bd8184902b2eb62e793
andi faizal <andifaizal@MacBook-Pro-andi.local>
2024-11-18 03:13:22 +0700

    add config

---
+++ components/screen/AppButton.tsx
@@ -0,0 +1,41 @@
+import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
+export const AppButton = ({ onPress, title, size, backgroundColor }: any) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={[
+      styles.appButtonContainer,
+      size === "sm" && {
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        elevation: 2,
+      },
+      backgroundColor && { backgroundColor },
+    ]}
+  >
+    <Text style={[styles.appButtonText, size === "sm" && { fontSize: 14 }]}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16,
+  },
+  appButtonContainer: {
+    elevation: 10,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+  },
+});
---
+++ components/screen/OtpCode.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import { AppButton } from "@/components/screen/AppButton";
+import { useState } from "react";
+
+const OtpCode = () => {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  return (
+    <ScrollView>
+      <View className="flex justify-center items-center mb-20">
+        <Image
+          source={require("../../assets/images/Image.png")}
+          style={[styles.image, { resizeMode: "contain" }]}
+        />
+      </View>
+      <View className="mx-5 mb-12">
+        <Text className="text-xl font-bold mb-4">OTP Verification</Text>
+        <Text className="text-lg font-medium text-[#B6B6B6]">
+          Enter email and phone number to send one time Password
+        </Text>
+        <Text>{email}</Text>
+      </View>
+      <View className="mx-5 mb-10">
+        <TextInput
+          style={{
+            height: 50,
+            borderColor: "gray",
+            borderWidth: 1,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+          }}
+          value={email}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+        />
+      </View>
+
+      <View className="mx-5 mb-16">
+        <TextInput
+          style={{
+            height: 50,
+            borderColor: "gray",
+            borderWidth: 1,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+          }}
+          value={phone}
+          onChangeText={(text) => {
+            setPhone(text);
+          }}
+        />
+      </View>
+
+      <View className="mx-5 ">
+        <AppButton title="Continue" size="sm" backgroundColor="#FF8D4D" />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    padding: 10,
+    height: 209,
+    width: 246,
+    marginTop: 100,
+  },
+});
+export default OtpCode;
